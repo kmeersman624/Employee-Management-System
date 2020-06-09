973a4423c00964ec3807af4c10cbbac91443c8e2
kmeersman <katie4682@yahoo.com>
2020-06-09 16:02:59 -0600

    updated .js and .sql files with starter code, dependecies and functions

---
+++ employeetracker.sql
@@ -0,0 +1,45 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE database employee_trackerDB;
+
+USE employee_trackerDB;
+
+-- department:
+-- id - INT PRIMARY KEY
+-- name - VARCHAR(30) to hold department name
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+-- role:
+-- id - INT PRIMARY KEY
+-- title - VARCHAR(30) to hold role title
+-- salary - DECIMAL to hold role salary
+-- department_id - INT to hold reference to department role belongs to
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,2) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+-- employee:
+-- id - INT PRIMARY KEY
+-- first_name - VARCHAR(30) to hold employee first name
+-- last_name - VARCHAR(30) to hold employee last name
+-- role_id - INT to hold reference to role employee has
+-- manager_id - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
